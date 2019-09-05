ed742acccc195b5988a11bb5ec11b36414558d78
uwishemajustine <alsenjsatine90@gmail.com>
2019-09-05 15:40:06 +0200

    add unittest to test me codes

---
+++ person.Info_test.py
@@ -0,0 +1,11 @@
+import unittest # Importing the unittest module
+from PersonInfo import Contact # Importing the contact class
+
+class TestPersonInfo(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
