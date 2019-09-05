db615dd5a50b0dba4e2aae9a1c4cb73724d72b2f
uwishemajustine <alsenjsatine90@gmail.com>
2019-09-05 15:46:38 +0200

    add first test to test if the object are correct

--- person.Info_test.py
+++ person.Info_test.py
@@ -9,3 +9,26 @@ class TestPersonInfo(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+
+     # Items up here .......
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_contact = Contact("James","Muriuki","0712345678","james@ms.com") # create contact object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual self.new_PersonInfo.login, "Maze")
+        self.assertEqual(self.new_PersonInfo.username,"Uwishema")
+        self.assertEqual(self.new_PersonInfo.password,"8844")
+        self.assertEqual(self.new_PersonInfo.email,"justine89@gmail.com")
+
+
+if __name__ == '__main__':
+    unittest.main()
