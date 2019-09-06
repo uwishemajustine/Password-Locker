590c0bcdffb9d57da80d3b981c2c351de5a4dd42
uwishemajustine <alsenjsatine90@gmail.com>
2019-09-06 09:54:15 +0200

    add third test using tearDown()  method which help to accurate test result

--- personinfo_test.py
+++ personinfo_test.py
@@ -4,19 +4,16 @@ from personinfo import personinfo
 class Testpersoninfo(unittest.TestCase):
 
     '''
-    Test class that defines test cases for the contact class behaviours.
+    Test class that defines test cases for the personinfo class behaviours.
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
-
-     # Items up here .......
-
     def setUp(self):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_personinfo = personinfo("Uwishema","8844","justine89@gmail.com") 
+        self.new_personinfo = personinfo("Uwishema","8882","uwishema10@gmail.com") 
 
 
     def test_init(self):
@@ -25,16 +22,33 @@ class Testpersoninfo(unittest.TestCase):
         '''
 
         self.assertEqual(self.new_personinfo.username,"Uwishema")
-        self.assertEqual(self.new_personinfo.password,"8844")
-        self.assertEqual(self.new_personinfo.email,"justine89@gmail.com")
+        self.assertEqual(self.new_personinfo.password,"8882")
+        self.assertEqual(self.new_personinfo.email,"uwishema10@gmail.com")
 
     def test_save_personinfo(self):
         '''
         test_save_personinfo test case to test if the personinfo object is saved into
-        the personinfo list
+         the personinfo list
         '''
         self.new_personinfo.save_personinfo() 
         self.assertEqual(len(personinfo.personinfo_list),1)
 
+    def tearDown(self):
+        '''
+        tearDown method that does clean up after each test case has run.
+        '''
+        personinfo.personinfo_list = []
+
+
+    def test_save_multiple_personinfo(self):
+        '''
+        test_save_multiple_personinfo to check if we can save multiple personinfo
+        objects to our personinfo_list
+        '''
+        self.new_personinfo.save_personinfo()
+        test_personinfo = personinfo("Uwishema","8882","uwishema10@gmail.com") 
+        test_personinfo.save_personinfo()
+        self.assertEqual(len(personinfo.personinfo_list),2)
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main() 
